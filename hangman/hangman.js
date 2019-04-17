946acaee305451562943cce96744c903fb538753
Caden Claussen <caden.claussen@gmail.com>
2019-04-16 17:04:06 -0700

    Hangman updates

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,10 +1,21 @@
+var input = require('readline-sync');
+
+
 var word = [];
-word[0] = 'c';
-word[1] = 'a';
-word[2] = 't';
+word[0] = 'b';
+word[1] = 'e';
+word[2] = 'a';
+word[3] = 'u';
+word[4] = 't';
+word[5] = 'i';
+word[6] = 'f';
+word[7] = 'u';
+word[8] = 'l';
 
 var answer = [];
 var solved = false;
+var hung = false;
+var misses = 0;
 
 
 function initAnswer() {
@@ -36,38 +47,96 @@ function isSolved() {
     return true;
 }
 
+
+function updateView(misses) {
+    if (misses > 0) {
+        console.log(' ');
+        drawStructure();
+    }
+    if (misses > 1) {
+        drawHead();
+    }
+    if (misses > 2) {
+        drawBody();
+    }
+    if (misses > 3) {
+        drawLeftArm();
+    }
+    if (misses > 4) {
+        drawRightArm();
+    }
+    if (misses > 5) {
+        drawLeftLeg();
+    }
+    if (misses > 6) {
+        drawRightLeg();
+    }
+}
+
+
+function drawStructure() {
+    console.log('Structure');
+}
+
+
+function drawHead() {
+    console.log('Head');
+}
+
+
+function drawBody() {
+    console.log('Body');
+}
+
+
+function drawLeftArm() {
+    console.log('LeftArm');
+}
+
+
+function drawRightArm() {
+    console.log('RightArm');
+}
+
+
+function drawLeftLeg() {
+    console.log('LeftLeg');
+}
+
+
+function drawRightLeg() {
+    console.log('RightLeg');
+}
+
+
 initAnswer(word);
-console.log(word);
-console.log(answer);
 
-found = guess('a');
-solved = isSolved();
-console.log('guess: a');
-console.log('found: ' + found);
-console.log('answer: ' + answer);
-console.log('solved: ' + solved);
-console.log(' ');
 
-found = guess('b');
-solved = isSolved();
-console.log('guess: b');
-console.log('found: ' + found);
-console.log('answer: ' + answer);
-console.log('solved: ' + solved);
-console.log(' ');
+while (solved === false && hung === false) {
 
-found = guess('t');
-solved = isSolved();
-console.log('guess: t');
-console.log('found: ' + found);
-console.log('answer: ' + answer);
-console.log('solved: ' + solved);
-console.log(' ');
+    console.log(' ');
+    console.log('answer: ' + answer);
+    console.log(' ');
 
-found = guess('c');
-solved = isSolved();
-console.log('guess: c');
-console.log('found: ' + found);
-console.log('answer: ' + answer);
-console.log('solved: ' + solved);
+    let letter = input.question('Guess a letter? ');
+
+    let found = guess(letter);
+
+    if (found === true) {
+        solved = isSolved();
+    } else {
+        misses = misses + 1;
+        if (misses === 7) {
+            hung = true;
+        }
+
+        updateView(misses);
+    }
+}
+
 console.log(' ');
+if (solved) {
+    console.log('Congratulations!');
+} else {
+    console.log('Sorry!');
+}
